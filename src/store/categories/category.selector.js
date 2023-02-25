b00d7911a386ea9cac62cd57f442d8f15095763a
Muhammad Rizqi Az Zayyad <muhammadrizqiaz@gmail.com>
2023-02-25 15:22:10 +0700

    migrate categories context code into redux

---
+++ src/store/categories/category.selector.js
@@ -0,0 +1,20 @@
+// The selector file is where this reducer specific transformation business logic is going to live.
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = state => state.categories;
+
+// Memoize selectors | which are very intelligent in being able to determine, hey, if nothing's changed, just don't even bother re rendering.
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  categoriesSlice => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  categories =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,29 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
+// make our own logger middleware (reusable middleware functions)
+const loggerMiddleware = store => next => action => {
+  // middleware signature | write the code that we want our middleware to do
+  if (!action.type) {
+    return next(action);
+  }
+  // console.log("type: ", action.type);
+  // console.log("payload: ", action.payload);
+
+  // Get state will give us back the value of the state right now.
+  // console.log("currentState: ", store.getState());
+
+  next(action);
+
+  // console.log("next state: ", store.getState());
+};
+
+const middleWares = [loggerMiddleware];
+
 // Middlewares our kind of like little library helpers that run before an action hits the reducer.
-const middleWares = [logger];
+// const middleWares = [logger];
 
 // Compose is a functional programming concept. It's essentially a way for us to pass multiple functions left to right.
 const composedEnhancers = compose(applyMiddleware(...middleWares));
