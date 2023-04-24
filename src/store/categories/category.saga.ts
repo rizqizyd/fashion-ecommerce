8dad86bfb33a13858477ff37aaf8de824c1a83aa
Muhammad Rizqi Az Zayyad <muhammadrizqiaz@gmail.com>
2023-04-24 11:02:33 +0700

    migrate redux-saga to typescript

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,4 +1,4 @@
-import { takeLatest, all, call, put } from "redux-saga/effects";
+import { takeLatest, all, call, put } from "typed-redux-saga/macro";
 
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
@@ -13,16 +13,16 @@ export function* fetchCategoriesAsync() {
   try {
     // this is an asynchronous event that happens and a wait is saying, Hey, I want you to wait until this comes back with something.
     // Anywhere you have a function and you want to turn it into an effect. You essentially use the call keyword.
-    const categoriesArray = yield call(getCategoriesAndDocuments, "categories");
-    yield put(fetchCategoriesSuccess(categoriesArray));
+    const categoriesArray = yield* call(getCategoriesAndDocuments);
+    yield* put(fetchCategoriesSuccess(categoriesArray));
   } catch (error) {
-    yield put(fetchCategoriesFailed(error));
+    yield* put(fetchCategoriesFailed(error as Error));
   }
 }
 
 export function* onFetchCategories() {
   // 'take' is where we receive actions. takeLatest means if you hear a bunch of the same action, give me the latest one.
-  yield takeLatest(
+  yield* takeLatest(
     CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START,
     fetchCategoriesAsync
   );
@@ -30,5 +30,5 @@ export function* onFetchCategories() {
 
 export function* categoriesSaga() {
   // 'all()' is essentially an effect that says, Hey, run everything inside and only complete when 'all' of it is done
-  yield all([call(onFetchCategories)]);
+  yield* all([call(onFetchCategories)]);
 }
--- src/store/root-saga.ts
+++ src/store/root-saga.ts
@@ -1,9 +1,9 @@
-import { all, call } from "redux-saga/effects";
+import { all, call } from "typed-redux-saga/macro";
 
 import { categoriesSaga } from "./categories/category.saga";
 import { userSagas } from "./user/user.saga";
 
 // export out a generator function. And that generator function is distinguished by this function signature word and then the star, that is a ES6 generator function.
 export function* rootSaga() {
-  yield all([call(categoriesSaga), call(userSagas)]);
+  yield* all([call(categoriesSaga), call(userSagas)]);
 }
